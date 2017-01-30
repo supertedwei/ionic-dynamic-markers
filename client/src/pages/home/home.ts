7b29f61a69d8f9a3ed6f00561181a5a9ff95c70f
Ted Wei <jdwei@hotmail.com>
2017-01-30 09:05:47 +0800

    Set up the Home Page

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { GoogleMaps } from '../../providers/google-maps';
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-    
-  }
-
+ 
+    @ViewChild('map') mapElement;
+    map: any;
+ 
+    constructor(public navCtrl: NavController, public maps: GoogleMaps) {
+ 
+    }
+ 
+    ionViewDidLoad(){
+        this.maps.initMap(this.mapElement.nativeElement);
+    }
+ 
 }
